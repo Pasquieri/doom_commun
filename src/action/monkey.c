a58d57a86c0e062c735767bf2053d3f644740059
Cyril J <cjulliar@e2r10p18.42.fr>
2019-04-27 17:08:49 +0200

    27avril2019 42 cyril . deplace monkey + attaque du singe

--- src/action/monkey.c
+++ src/action/monkey.c
@@ -14,21 +14,46 @@
 
 void	attack(t_env *env, int i)
 {
-	/*printf("M:%.1f - %.1f\n", env->sp[4].sprite[i].cd.x, env->sp[4].sprite[i].cd.y);
-	printf("J:%.1f - %.1f\n", env->perso_x, env->perso_y);
-	printf("CM:(%.1f;%.1f)\n", env->sp[4].sprite[i].cd.x / env->coef, env->sp[4].sprite[i].cd.y / env->coef);
-	*/if (env->sp[4].sprite[i].cd.x / env->coef != env->perso_x / env->coef && env->sp[4].sprite[i].cd.y / env->coef != env->perso_y / env->coef)
+	int mJx = (env->perso_x / env->coef) / 1;
+	int mJy = (env->perso_y / env->coef) / 1;
+	int x;
+	int y;
+	x = 0;
+	y = 0;
+
+	if (((int)(env->sp[4].sprite[i].cd.x-3) / env->coef) / 1 == mJx)
+		x = 1;
+	else if(((int)(env->sp[4].sprite[i].cd.x+3) / env->coef) / 1 == mJx)
+		x = 1;
+	if (((int)(env->sp[4].sprite[i].cd.y-3) / env->coef) / 1 == mJy)
+		y = 1;
+	else if (((int)(env->sp[4].sprite[i].cd.y+3) / env->coef) / 1 == mJy)
+		y = 1;
+	if (x && y)
+		env->h_life -= 2;
+		
+}
+
+void	runToPlayer(t_env *env, int i) // colision mur a add
+{
+	int mJx = (env->perso_x / env->coef) / 1;
+	int mJy = (env->perso_y / env->coef) / 1;
+
+	if ((((env->sp[4].sprite[i].cd.x-1) / env->coef) / 1 == mJx))
+		printf("ERROR: %.1f - %d\n",((env->sp[4].sprite[i].cd.x-1) / env->coef) / 1, mJx );
+	if (env->sp[4].sprite[i].cd.x / env->coef != env->perso_x / env->coef &&
+	 env->sp[4].sprite[i].cd.y / env->coef != env->perso_y / env->coef)
 	{
-		//env->tab[(int)(env->sp[4].sprite[i].cd.y / env->coef)][(int)(env->sp[4].sprite[i].cd.x / env->coef)] = 0;
-		if (env->sp[4].sprite[i].cd.x / env->coef > env->perso_x / env->coef)// && 
-		//	env->[env->sp[4].sprite[i].cd.x+1 / env->coef][env->sp[4].sprite[i].cd.y / env->coef])
-			env->sp[4].sprite[i].cd.x -= 1;
-		else
-			env->sp[4].sprite[i].cd.x += 1;
-		if (env->sp[4].sprite[i].cd.y / env->coef > env->perso_y / env->coef)
-			env->sp[4].sprite[i].cd.y -= 1;
-		else
-			env->sp[4].sprite[i].cd.y += 1;
+		if (env->sp[4].sprite[i].cd.x / env->coef > env->perso_x / env->coef 
+			&& (((int)(env->sp[4].sprite[i].cd.x-1) / env->coef) / 1 != mJx))
+			env->sp[4].sprite[i].cd.x -= 0.3;
+		else if(((int)(env->sp[4].sprite[i].cd.x+1) / env->coef) / 1 != mJx)
+			env->sp[4].sprite[i].cd.x += 0.3;
+		if (env->sp[4].sprite[i].cd.y / env->coef > env->perso_y / env->coef
+			&& (((int)(env->sp[4].sprite[i].cd.y-1) / env->coef) / 1 != mJy))
+			env->sp[4].sprite[i].cd.y -= 0.3;
+		else if (((int)(env->sp[4].sprite[i].cd.y+1) / env->coef) / 1 != mJy)
+			env->sp[4].sprite[i].cd.y += 0.3;
 		env->tab[(int)(env->sp[4].sprite[i].cd.y / env->coef)][(int)(env->sp[4].sprite[i].cd.x / env->coef)] = 14;
 	}
 }
@@ -41,8 +66,10 @@ void	ft_monkey(t_env *env)
 	int i = 0;
 	while (i < k)
 	{
-		if (env->sp[4].sprite[i].det == 1 && env->time % 10 == 0)
+		if (env->time % 10 == 0)
 			attack(env, i);
+		if (env->sp[4].sprite[i].det == 1 && env->time % 1 == 0)
+			runToPlayer(env, i);
 		i++;
 	}
 }
