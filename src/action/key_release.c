9769ff6ead9ebff376226075cdcdde83b4b867ea
Clara PALMIERI <cpalmier@e1r5p12.42.fr>
2019-05-01 21:17:18 +0200

    tir

--- src/action/key_release.c
+++ src/action/key_release.c
@@ -6,7 +6,7 @@
 /*   By: mpasquie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 15:19:17 by mpasquie          #+#    #+#             */
-/*   Updated: 2019/05/01 20:58:29 by cpalmier         ###   ########.fr       */
+/*   Updated: 2019/05/01 21:16:59 by cpalmier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	key_gun(int key, t_env *env)
 {
-	if (key == 269 && env->gun == 1 && !env->menu && env->h_ammo > 0)
+	if (key == 258 && env->gun == 1 && !env->menu && env->h_ammo > 0)
 	{
 		print_gun(env, 5, 9);
 		mlx_put_image_to_window(env->mlx, env->win, env->m[0].img, 0, 0);
